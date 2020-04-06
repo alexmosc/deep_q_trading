29b9f14f64f562f1cc3b8f1947d588360bd66d2e
alexmosc <burnakov@yandex.ru>
2020-04-06 14:12:11 +0300

    bug fixed with data.table initialization

--- LOGS_class.R
+++ LOGS_class.R
@@ -4,13 +4,20 @@ library(R6)
 Logs <- R6::R6Class(
 	'Data'
 	, public = list(
-		q_val_tracker = data.table()
-		, critic_loss_tracker = numeric()
-		, returns_data = data.table()
-		, reward_buffer = numeric()
+		q_val_tracker = NULL
+		, critic_loss_tracker = NULL
+		, returns_data = NULL
+		, reward_buffer = NULL
 		
 		, initialize = function()
 		{
+			
+			require(data.table)
+			
+			self$q_val_tracker <- data.table()
+			self$critic_loss_tracker <- numeric()
+			self$returns_data <- data.table()
+			self$reward_buffer <- numeric()
 			
 			cat('########## loggin was initialized ##########', '\n')
 			
